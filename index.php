082f584029b6fe65e45c69278e66e50f4b8bd181
Shashi Shekhar <72846752+shashishekhar-robotist@users.noreply.github.com>
2021-08-25 10:51:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 <?php
 echo "My first PHP script!<br>";
 $txt = "PHP";
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
 echo "I love $txt!<br>";
 ECHO "Hello World!<br>";
 echo "Hello World!<br>";
