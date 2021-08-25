dfa8b7234e646685ea8b7e3189054761ca69a1be
Shashi Shekhar <72846752+shashishekhar-robotist@users.noreply.github.com>
2021-08-25 11:11:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,17 @@ echo "I love $txt!<br>";
 ECHO "Hello World!<br>";
 echo "Hello World!<br>";
 EcHo "Hello World!<br>";
+$x = 5;
+$y = 10;
+
+function myTest() {
+  global $x, $y;
+  $y = $x + $y;
+} 
+
+myTest();  // run function
+echo $y; // output the new value for variable $y
+?>
 ?>
 
 </body>
